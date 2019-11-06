036faea2c4b2f60448b514ff1c0b41bf4a5ab498
Vikas Adicherla <vikas.adicherla@gmail.com>
2019-11-06 15:01:50 +0530

    kubernates intial commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Your Watching App deployed on AWS</h1>
+            <h1 className="App-title">Working with Kubernates Clusters</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
